35cab33d30a365442571d2b1a78619ce2ed32745
caarlosroosa2003 <119525632+caarlosroosa2003@users.noreply.github.com>
2024-02-18 23:38:36 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFeather } from '@fortawesome/free-solid-svg-icons';
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, FontAwesomeModule, HeaderComponent]
 })
 export class AppComponent {
   title = 'blogs';
+  faFeather = faFeather;
 }
